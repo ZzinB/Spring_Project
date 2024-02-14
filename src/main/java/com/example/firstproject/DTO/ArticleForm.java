b088b543b19b93d4d624c63a4ea7fdd6c29f50a4
ZzinB <mysteleee7@gmail.com>
2024-02-14 14:51:56 +0900

    Refac: ArticleForm에 lombok적용

--- src/main/java/com/example/firstproject/DTO/ArticleForm.java
+++ src/main/java/com/example/firstproject/DTO/ArticleForm.java
@@ -1,25 +1,15 @@
 package com.example.firstproject.DTO;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title; //제목 필드
     private String content; //내용 필드
 
-    //생성자
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
