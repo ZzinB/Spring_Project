b5eeac665364fd8049977c10ed8312d0c07c0f53
ZzinB <mysteleee7@gmail.com>
2024-02-14 15:46:23 +0900

    Feat: 글 수정 기능 (update)

--- src/main/java/com/example/firstproject/DTO/ArticleForm.java
+++ src/main/java/com/example/firstproject/DTO/ArticleForm.java
@@ -7,11 +7,12 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class ArticleForm {
+    private Long id; //아이디 필드
     private String title; //제목 필드
     private String content; //내용 필드
 
 
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }
--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -73,4 +73,23 @@ public class ArticleController {
         return "articles/edit";
     }
 
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. 엔티티를 DB에 저장
+        // 2-1. DB에서 기존 데이터 가져오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        // 2-2. 기존 데이터 값 갱신
+        if(target != null){
+            articleRepository.save(articleEntity);
+        }
+
+        // 3. 수정 결과 페이지로 리다이렉트
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
 }
