07266f17ca3c898e148f0fab11f9d86b99b7397d
ZzinB <mysteleee7@gmail.com>
2024-02-14 15:09:31 +0900

    Feat: 데이터 조회

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,9 +40,14 @@ public class ArticleController {
 
     //Read
     @GetMapping("/articles/{id}")
-    public String show(@PathVariable Long id){
+    public String show(@PathVariable Long id, Model model){
         log.info("id = " + id);
-        return "";
+        // 1. id 조회해서 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 반환
+        return "articles/show";
     }
 
 
