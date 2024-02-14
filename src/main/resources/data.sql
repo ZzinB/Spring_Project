0d57cf3dbf44bcf7098bbf99fb2e06f1dc804776
ZzinB <mysteleee7@gmail.com>
2024-02-14 15:36:10 +0900

    Add: db 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id, title, content) VALUES (1, '가', '11');
+INSERT INTO article(id, title, content) VALUES (2, '나', '22');
+INSERT INTO article(id, title, content) VALUES (3, '다', '33');
