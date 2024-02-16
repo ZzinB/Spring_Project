e29f289ff89eb675424ffec559a0fb7022c2316b
ZzinB <mysteleee7@gmail.com>
2024-02-16 15:20:01 +0900

    Add: 댓글 삭제 (Delete)

--- CRUD/src/main/java/com/example/firstproject/api/CommentApiController.java
+++ CRUD/src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -41,4 +41,11 @@ public class CommentApiController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
     }
     // 4. 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    private ResponseEntity<CommentDto> delete(@PathVariable Long id){
+        // 서비스에 위임
+        CommentDto deletedDto = commentService.delete(id);
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(deletedDto);
+    }
 }
