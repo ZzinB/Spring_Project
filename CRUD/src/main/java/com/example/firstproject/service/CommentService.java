12de48e731db2a87821810c65c30c7b1fb6a79ea
ZzinB <mysteleee7@gmail.com>
2024-02-16 12:21:31 +0900

    Refac: 스트림사용

--- CRUD/src/main/java/com/example/firstproject/service/CommentService.java
+++ CRUD/src/main/java/com/example/firstproject/service/CommentService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -18,16 +19,19 @@ public class CommentService {
     private ArticleRepository articleRepository;
 
     public List<CommentDto> comments(Long articleId) {
-        // 1. 댓글 조회
-        List<Comment> comments = commentRepository.findByArticleId(articleId);
-        // 2. 엔티티 -> DTO 변환
-        List<CommentDto> dtos = new ArrayList<CommentDto>();
-        for (int i=0 ; i<comments.size() ; i++){
-            Comment c = comments.get(i);
-            CommentDto dto = CommentDto.createCommentDto(c);  //엔티티를 DTO로 변롼
-            dtos.add(dto);  // 변환환 DTO를 dtos 리스트에 삽입
-        }
+//        // 1. 댓글 조회
+//        List<Comment> comments = commentRepository.findByArticleId(articleId);
+//        // 2. 엔티티 -> DTO 변환
+//        List<CommentDto> dtos = new ArrayList<CommentDto>();
+//        for (int i=0 ; i<comments.size() ; i++){
+//            Comment c = comments.get(i);
+//            CommentDto dto = CommentDto.createCommentDto(c);  //엔티티를 DTO로 변롼
+//            dtos.add(dto);  // 변환환 DTO를 dtos 리스트에 삽입
+//        }
         // 3. 결과 반환
-        return  dtos;
+        return  commentRepository.findByArticleId(articleId) //댓글 엔티티 목록 조회
+                .stream()  // 댓글 엔티티 목록을 스트림으로 변환
+                .map(comment -> CommentDto.createCommentDto(comment))  //엔티티를 DTO로 매핑
+                .collect(Collectors.toList());  //스트림을 리스트로 변환
     }
 }
