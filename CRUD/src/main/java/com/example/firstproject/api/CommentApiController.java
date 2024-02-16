0c945afcf4731e2ef3b27d4d92770e4b5a1cb1ec
ZzinB <mysteleee7@gmail.com>
2024-02-16 15:15:08 +0900

    Add: 댓글 수정 (Patch)

--- CRUD/src/main/java/com/example/firstproject/api/CommentApiController.java
+++ CRUD/src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -33,6 +33,12 @@ public class CommentApiController {
     }
 
     // 3. 댓글 수정
-
+    @PatchMapping("/api/comments/{id}")
+    private ResponseEntity<CommentDto> update(@PathVariable Long id, @RequestBody CommentDto dto){
+        // 서비스에 위임
+        CommentDto updatedDto = commentService.update(id, dto);
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
+    }
     // 4. 댓글 삭제
 }
