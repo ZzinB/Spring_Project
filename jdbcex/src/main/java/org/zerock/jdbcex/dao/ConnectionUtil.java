5bff539caaa2b1b976907016925a66f5f4a4315d
ZzinB <mysteleee7@gmail.com>
2024-03-04 22:23:18 +0900

    Style: Connection Pool

--- jdbcex/src/main/java/org/zerock/jdbcex/dao/ConnectionUtil.java
+++ jdbcex/src/main/java/org/zerock/jdbcex/dao/ConnectionUtil.java
@@ -22,7 +22,7 @@ public enum ConnectionUtil {
 
         ds = new HikariDataSource(config);
     }
-    public Connection getConnection() throws Exception{
+    public Connection getConnection()throws Exception{
         return ds.getConnection();
     }
 }
