8a845c2fce897e4967f1aacf9f614999ded1c2ef
ZzinB <mysteleee7@gmail.com>
2024-03-06 21:08:39 +0900

    Add: TodoDTO, TodoVO 연결하는 ModelMapper 사용

---
+++ jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -0,0 +1,26 @@
+package org.zerock.jdbcex.service;
+
+import org.modelmapper.ModelMapper;
+import org.zerock.jdbcex.dao.TodoDAO;
+import org.zerock.jdbcex.domain.TodoVO;
+import org.zerock.jdbcex.dto.TodoDTO;
+import org.zerock.jdbcex.util.MapperUtil;
+
+public enum TodoService {
+    INSTANCE;
+
+    private TodoDAO dao;
+    private ModelMapper modelMapper;
+
+    TodoService() {
+        dao = new TodoDAO();
+        modelMapper = MapperUtil.INSTANCE.get();
+    }
+
+    //TodoDTO를 파라미터로 받아서 TodoVO로 변환하는 과정
+    public void register(TodoDTO todoDTO) throws Exception{
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+        System.out.println("todoVO = " + todoVO);
+        dao.insert(todoVO);
+    }
+}
