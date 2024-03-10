75bc1cd357eabb1fea2fb8c099933d2a9c4c4edc
ZzinB <mysteleee7@gmail.com>
2024-03-10 21:18:45 +0900

    Feat: 세션을 이용한 로그인 기능

--- w2/src/main/java/org/zerock/jdbcex/controller/TodoRegisterController.java
+++ w2/src/main/java/org/zerock/jdbcex/controller/TodoRegisterController.java
@@ -24,6 +24,23 @@ public class TodoRegisterController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("/todo/register GET ....");
 
+        HttpSession session = req.getSession();
+
+        //기존에 JSESSIONID가 없는 새로운 사용자
+        if(session.isNew()){
+            log.info("JSESSIONID 쿠키가 새로 만들어진 사용자");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        //JSESSIONID가 있지만 해당 세션 컨텍스트에 loginInfo라는 이름으로 저장된 객체가 없는 경우
+        if(session.getAttribute("loginIndo") == null){
+            log.info("로그인한 정보가 없는 사용자.");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        //정상적인 경우
         req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
     }
 
