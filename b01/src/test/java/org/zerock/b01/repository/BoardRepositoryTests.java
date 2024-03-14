c5c0951ed58abff50da78626338f201262fab3f0
ZzinB <mysteleee7@gmail.com>
2024-03-14 17:26:34 +0900

    Test: crud 테스트

---
+++ b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -0,0 +1,68 @@
+package org.zerock.b01.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.b01.domain.Board;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    //저장
+    @Test
+    public void testInsert(){
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Board board = Board.builder()
+                    .title("title..."+i)
+                    .content("content..."+i)
+                    .writer("user"+(i%10))
+                    .build();
+
+            Board result = boardRepository.save(board);
+            log.info("BNO: " + result.getBno());
+        });
+    }
+
+    //select 기능
+    @Test
+    public void testSelect(){
+        Long bno = 100L;
+
+        Optional<Board> result = boardRepository.findById(bno);
+
+        Board board = result.orElseThrow();
+
+        log.info(board);
+    }
+
+    //update
+    @Test
+    public void testUpdate(){
+        Long bno = 100L;
+
+        Optional<Board> result = boardRepository.findById(bno);
+
+        Board board = result.orElseThrow();
+
+        board.change("update.. title 100", "update.. content 100");
+
+        boardRepository.save(board);
+    }
+
+    //delete
+    @Test
+    public void testDelete(){
+        Long bno = 1L;
+
+        boardRepository.deleteById(bno);
+    }
+}
