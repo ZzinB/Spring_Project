44f975e134057884e8266ae51c7fd46c0383d9a5
ZzinB <mysteleee7@gmail.com>
2024-03-14 20:13:54 +0900

    Add: Querydsl 적용중

--- b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -76,4 +76,12 @@ public class BoardRepositoryTests {
         Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
         Page<Board> result = boardRepository.findAll(pageable);
     }
+
+    @Test
+    public void testSearch1(){
+        //2. page order by bno desc
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("bno").descending());
+
+        boardRepository.search1(pageable);
+    }
 }
