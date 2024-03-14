32d67b4b03c0a056039395b9ee2bd9cbcdbaadc3
ZzinB <mysteleee7@gmail.com>
2024-03-14 17:13:06 +0900

    Add: 엔티티

--- b01/src/main/java/org/zerock/b01/B01Application.java
+++ b01/src/main/java/org/zerock/b01/B01Application.java
@@ -2,8 +2,10 @@ package org.zerock.b01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  //AudtingEntityListener 활성화 시키기 위한 애노테이션
 public class B01Application {
 
     public static void main(String[] args) {
---
+++ b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -0,0 +1,26 @@
+package org.zerock.b01.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 500, nullable = false)  //column 길이와 null 허용 여부
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
