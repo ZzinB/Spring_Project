5a2ce329a43187318810959962ebb6f3a2ad9c5d
ZzinB <mysteleee7@gmail.com>
2024-03-15 15:32:25 +0900

    Add: 수정

--- b01/src/main/java/org/zerock/b01/service/BoardServiceImpl.java
+++ b01/src/main/java/org/zerock/b01/service/BoardServiceImpl.java
@@ -40,4 +40,14 @@ public class BoardServiceImpl implements BoardService{
 
         return boardDTO;
     }
+
+    @Override
+    public void modify(BoardDTO boardDTO) {
+        Optional<Board> result = boardRepository.findById(boardDTO.getBno());
+
+        Board board = result.orElseThrow();
+
+        board.change(boardDTO.getTitle(), boardDTO.getContent());
+        boardRepository.save(board);
+    }
 }
