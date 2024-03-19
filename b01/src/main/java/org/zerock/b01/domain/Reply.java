256554a1f91e15e3c3145468cf1154ecdcba6cf8
ZzinB <mysteleee7@gmail.com>
2024-03-19 21:45:46 +0900

    Add: 인덱스 생성 (@Table)
    게시물 번호를 쿼리 조건으로 자주 사용하므로 인덱스 지정

--- b01/src/main/java/org/zerock/b01/domain/Reply.java
+++ b01/src/main/java/org/zerock/b01/domain/Reply.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "reply", indexes = {@Index(name = "idx_reply_board_bno", columnList = "board_bno")}) //인덱스 생성
 @Getter
 @Builder
 @AllArgsConstructor
