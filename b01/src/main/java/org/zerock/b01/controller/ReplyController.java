1c5f341c0def98b3ae87f48e96e166d824aeb01f
ZzinB <mysteleee7@gmail.com>
2024-03-20 16:48:08 +0900

    Add: 댓글 목록 api, 댓글 조회 api

--- b01/src/main/java/org/zerock/b01/controller/ReplyController.java
+++ b01/src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -1,5 +1,6 @@
 package org.zerock.b01.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -7,10 +8,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.dto.ReplyDTO;
 import org.zerock.b01.service.ReplyService;
 
@@ -26,7 +26,7 @@ public class ReplyController {
 
     private final ReplyService replyService;
 
-    @ApiOperation(value = "Replies POST", notes = "POST 방식응로 댓글 등록")
+    @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, Long> register(@Valid @RequestBody ReplyDTO replyDTO,
                                       BindingResult bindingResult) throws BindException {
@@ -43,5 +43,19 @@ public class ReplyController {
         resultMap.put("rno", 111L);
 
         return resultMap;
+    }
+
+    @ApiOperation(value = "Replies of Board", notes = "GET 방식으로 특정 게시물의 댓글 목록")
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno, PageRequestDTO pageRequestDTO){
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno, pageRequestDTO);
+        return responseDTO;
+    }
+
+    @ApiOperation(value = "Read Reply", notes = "GET 방식으로 특정 댓글 조회")
+    @GetMapping("/{rno}")
+    public ReplyDTO getReplyDTO(@PathVariable("rno") Long rno){
+        ReplyDTO replyDTO = replyService.read(rno);
+        return replyDTO;
     }
 }
