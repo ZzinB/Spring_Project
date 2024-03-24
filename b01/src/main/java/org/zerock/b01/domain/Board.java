aeafd62caec09780ad90f5696595d6e32e7eb25c
ZzinB <mysteleee7@gmail.com>
2024-03-24 21:29:55 +0900

    Add: orphanRemoval

--- b01/src/main/java/org/zerock/b01/domain/Board.java
+++ b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -35,7 +35,8 @@ public class Board extends BaseEntity{
     //양방향(bidirectiona)참조
     @OneToMany(mappedBy = "board",        //BoardImage의 board 변수
             cascade = {CascadeType.ALL},  //Board 엔티티 객체 모든 상태 변화에 BoardImage 객체도 같이 변경
-            fetch = FetchType.LAZY)  //BoardImage의 board 변수
+            fetch = FetchType.LAZY,
+            orphanRemoval = true)    //상위 엔티티의 상태 변화 (삭제) 하위 엔티티 삭제까지 진행시켜!
     @Builder.Default
     private Set<BoardImage> imageSet = new HashSet<>();
 
