a9e1485b4453cf0303262b3236f031acf6b131ba
ZzinB <mysteleee7@gmail.com>
2024-03-24 21:10:23 +0900

    Test: 게시물에 첨부파일 여러개 테스트

--- b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -13,6 +13,7 @@ import org.zerock.b01.dto.BoardListReplyCountDTO;
 
 import javax.swing.text.html.Option;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -142,5 +143,20 @@ public class BoardRepositoryTests {
 
         result.getContent().forEach(board -> log.info(board));
 
+    }
+
+    @Test
+    public void testInsertWithImage(){
+        Board board = Board.builder()
+                .title("Image Test")
+                .content("첨부파일 테스트")
+                .writer("tester")
+                .build();
+
+        for(int i=0 ; i<3 ; i++){
+            board.addImage(UUID.randomUUID().toString(), "file" + i + ".jpg");
+        }
+
+        boardRepository.save(board);
     }
 }
