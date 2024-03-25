f7ef514603891b7ad220da4d7b560ed116165a3f
ZzinB <mysteleee7@gmail.com>
2024-03-25 13:46:49 +0900

    Add: dtoToEntity

--- b01/src/main/java/org/zerock/b01/service/BoardService.java
+++ b01/src/main/java/org/zerock/b01/service/BoardService.java
@@ -1,5 +1,6 @@
 package org.zerock.b01.service;
 
+import org.zerock.b01.domain.Board;
 import org.zerock.b01.dto.*;
 
 public interface BoardService {
@@ -18,4 +19,23 @@ public interface BoardService {
 
     //게시글의 이미지와 댓글의 숫자까지 처리
     PageResponseDTO<BoardListAllDTO> listWithAll(PageRequestDTO pageRequestDTO);
+
+    //DTO 객체를 엔티티 객체로 변환
+    //BoardDTO의 List<String> fileNames를 Board의 Set<BoardImage>로 변환
+    default Board dtoToEntity(BoardDTO boardDTO){
+        Board board = Board.builder()
+                .bno(boardDTO.getBno())
+                .title(boardDTO.getTitle())
+                .content(boardDTO.getContent())
+                .writer(boardDTO.getWriter())
+                .build();
+
+        if(boardDTO.getFileNames() != null){
+            boardDTO.getFileNames().forEach(fileName->{
+                String[] arr = fileName.split("_");
+                board.addImage(arr[0], arr[1]);
+            });
+        }
+        return board;
+    }
 }
