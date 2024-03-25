a3632f6e34079c8c99a856b261965f086d2e0eb3
ZzinB <mysteleee7@gmail.com>
2024-03-25 15:50:27 +0900

    Add: 자동로그인

--- b01/src/main/java/org/zerock/b01/config/CustomSecurityConfig.java
+++ b01/src/main/java/org/zerock/b01/config/CustomSecurityConfig.java
@@ -11,11 +11,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.zerock.b01.security.CustomUserDetailsService;
+
+import javax.sql.DataSource;
 
 @Log4j2
 @Configuration
+@RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)  //권한 설정
 public class CustomSecurityConfig {
+
+    private final DataSource dataSource;
+    private final CustomUserDetailsService userDetailsService;
 
     //정적 자원에 시큐리티 적용 제외
     @Bean
@@ -32,8 +41,24 @@ public class CustomSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         log.info("------------configure----------");
+        //커스텀 로그인 페이지
         http.formLogin().loginPage("/member/login");
-        http.csrf().disable();  //CSRF토큰 비활성화
+        //CSRF토큰 비활성화
+        http.csrf().disable();
+
+        //자동 로그인
+        http.rememberMe()
+                .key("12345678")
+                .tokenRepository(persistentTokenRepository())
+                .userDetailsService(userDetailsService)
+                .tokenValiditySeconds(60*60*24*30);
         return http.build();
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(){
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
     }
 }
