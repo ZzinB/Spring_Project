39eda28e71c6353e57df8afcd06781ea1052cba5
ZzinB <mysteleee7@gmail.com>
2024-03-25 13:55:32 +0900

    Test: 조회 테스트

--- b01/src/test/java/org/zerock/b01/service/BoardServiceImplTest.java
+++ b01/src/test/java/org/zerock/b01/service/BoardServiceImplTest.java
@@ -82,4 +82,17 @@ class BoardServiceImplTest {
         Long bno = boardService.register(boardDTO);
         log.info("bno: " + bno);
     }
+
+    @Test
+    public void testReadAll(){
+        Long bno = 101L;
+
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        log.info(boardDTO);
+
+        for (String fileName : boardDTO.getFileNames()){
+            log.info(fileName);
+        }
+    }
 }
