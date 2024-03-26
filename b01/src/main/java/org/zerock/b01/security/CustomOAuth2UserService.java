3637d97385c366f933cef3f24e7917cf28d45980
ZzinB <mysteleee7@gmail.com>
2024-03-26 15:54:43 +0900

    Add: 소셜 로그인과 일반 로그인 로그인 분리

--- b01/src/main/java/org/zerock/b01/security/CustomOAuth2UserService.java
+++ b01/src/main/java/org/zerock/b01/security/CustomOAuth2UserService.java
@@ -2,21 +2,30 @@ package org.zerock.b01.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.zerock.b01.domain.Member;
+import org.zerock.b01.domain.MemberRole;
+import org.zerock.b01.repository.MemberRepository;
+import org.zerock.b01.security.dto.MemberSecurityDTO;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService  extends DefaultOAuth2UserService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         log.info("userRequest ...");
@@ -44,6 +53,40 @@ public class CustomOAuth2UserService  extends DefaultOAuth2UserService {
         log.info("==================================");
 
         return oAuth2User;
+    }
+
+    private MemberSecurityDTO generateDTO(String email, Map<String, Object> params){
+        Optional<Member> result = memberRepository.findByEmail(email);
+
+        //db에 해당 이메일 사용자가 없다면
+        if(result.isEmpty()){
+            //회원 추가
+            Member member = Member.builder()
+                    .mid(email)
+                    .mpw(passwordEncoder.encode("1111"))
+                    .email(email)
+                    .social(true)
+                    .build();
+            member.addRole(MemberRole.USER);
+            memberRepository.save(member);
+
+            //MemberSecurityDTO 구성, 반환
+            MemberSecurityDTO memberSecurityDTO = new MemberSecurityDTO(email, "1111", email, false, true, Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+            memberSecurityDTO.setProps(params);
+            return memberSecurityDTO;
+        }else {
+            Member member = result.get();
+            MemberSecurityDTO memberSecurityDTO = new MemberSecurityDTO(
+                    member.getMid(),
+                    member.getMpw(),
+                    member.getEmail(),
+                    member.isDel(),
+                    member.isSocial(),
+                    member.getRoleSet().stream().map(memberRole -> new SimpleGrantedAuthority("ROLE_" + memberRole.name()))
+                            .collect(Collectors.toList())
+            );
+            return memberSecurityDTO;
+        }
     }
 
     private String getKaKaoEmail(Map<String, Object> paramMap){
