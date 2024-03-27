ba051371d4286d51c37c408eb61d8976cae2fffd
ZzinB <mysteleee7@gmail.com>
2024-03-27 14:22:20 +0900

    Add: APILoginFilter JSON 처리
    mid, mpw 값 분리 및 Axios 이용 전달

--- api01/src/main/java/org/zerock/api01/security/filter/APILoginFilter.java
+++ api01/src/main/java/org/zerock/api01/security/filter/APILoginFilter.java
@@ -1,5 +1,6 @@
 package org.zerock.api01.security.filter;
 
+import com.google.gson.Gson;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
@@ -10,6 +11,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
 
 
 //인증 단계 처리
@@ -24,6 +28,27 @@ public class APILoginFilter extends AbstractAuthenticationProcessingFilter {
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
         log.info("APILoginFilter ....");
+
+        if(request.getMethod().equalsIgnoreCase("GET")){
+            log.info("GET METHOD NOTE SUPPORT");
+            return null;
+        }
+
+        Map<String, String> jsonData = parseRequestJSON(request);
+
+        log.info(jsonData);
+        return null;
+    }
+
+    private Map<String , String > parseRequestJSON(HttpServletRequest request){
+        //JSON 데이터를 분석해서 mid, mpw 전달 값으로 Map으로 처리
+        try (Reader reader = new InputStreamReader(request.getInputStream())){
+            Gson gson = new Gson();
+
+            return gson.fromJson(reader, Map.class);
+        } catch (Exception e){
+            log.error(e.getMessage());
+        }
         return null;
     }
 }
