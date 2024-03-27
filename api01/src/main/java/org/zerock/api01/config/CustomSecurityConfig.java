d38e81106c40861a9976f578041fd37e46e34ab6
ZzinB <mysteleee7@gmail.com>
2024-03-27 14:28:22 +0900

    Add: 인증 성공 처리

--- api01/src/main/java/org/zerock/api01/config/CustomSecurityConfig.java
+++ api01/src/main/java/org/zerock/api01/config/CustomSecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.zerock.api01.security.APIUserDetailsService;
 import org.zerock.api01.security.filter.APILoginFilter;
+import org.zerock.api01.security.handle.APILoginSuccessHandler;
 
 
 @Configuration
@@ -62,6 +63,12 @@ public class CustomSecurityConfig {
         //APILoginFilter
         APILoginFilter apiLoginFilter = new APILoginFilter("/generateToken");
         apiLoginFilter.setAuthenticationManager(authenticationManager);
+
+        //APILoginSuccessHandler
+        APILoginSuccessHandler successHandler = new APILoginSuccessHandler();
+        //SuccessHandler 실행
+        apiLoginFilter.setAuthenticationSuccessHandler(successHandler);
+
 
         //APILoginFilter 위치
         http.addFilterBefore(apiLoginFilter, UsernamePasswordAuthenticationFilter.class);
